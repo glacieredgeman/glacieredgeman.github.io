9f2ed01559b8f608f18a52e4f6f1925dd5e2eed7
Highlander <albertzyang@gmail.com>
2021-03-27 17:40:40 -0400

    pulse working

--- js/server.js
+++ js/server.js
@@ -2,6 +2,10 @@ var http = require('http');
 var url = require('url');  
 var fs = require('fs');  
 var server = http.createServer(function(request, response) {  
+    router.css(request, response);
+    router.home(request, response);
+    router.user(request, response);
+    
     var path = request.url.toString();  
     // console.log(request.url);
     switch (path) {  
